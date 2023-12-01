28564c3645ce7c1041629b572b011f5a9eff5b54
Alexa Gomez <71423472+anatagomez@users.noreply.github.com>
2023-12-02 00:01:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,4 @@
-import camelot
-
-
-class pdf2text:
-    def __init__(self):
-        pass
-
-    def __call__(self, pdf_file):
-        tables = camelot.read_pdf(pdf_file, pages="all")
-        texts = []
-        for table in tables:
-            texts.append(table.df.to_string())
-        text = "\n".join(texts)
-        text = text.replace("\\n", "")
-        return text
-
-
-class text2vector:
-    def __init__(self):
-        pass
-
-    def __call__(self, text):
-        pass
-
-
-class cosine_sim:
-    def __init__(self):
-        pass
-
-    def __call__(self, vector_from_table, vector_from_keyword):
-        pass
+git@github.com:anatagomez/39.git
 
 
 def main(keyword, pdf_file):
